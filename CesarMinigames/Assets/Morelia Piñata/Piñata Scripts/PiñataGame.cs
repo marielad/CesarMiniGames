4571cbb6f00b4210648b4711241155170bd30e60
Morelia Vera <moreliaveraruiz@gmail.com>
2021-12-09 12:18:22 +0000

    Animaciones hechas
    estan hechas las animaciones, el script funciona falta pulirlo hacerlo mas limpio, preguntar sobre los niveles

---
+++ CesarMinigames/Assets/Morelia Piñata/Piñata Scripts/PiñataGame.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class PiñataGame : MonoBehaviour
+{
+    public float timer;
+    public bool startTimer = false;
+    public float stopTime;
+    public int numPiñata;
+    public int numPiñataDestruida;
+    public int numPulsaciones;
+    public int numPulsacionesNecesarias;
+
+    public TextMeshProUGUI timerText;
+    public TextMeshProUGUI pinatasDestroyedText;
+    public TextMeshProUGUI pinatasLeftText;
+
+    public Sprite burro, burroRoto, estrella, estrellaRota, pinata, pinataRota;
+    public Sprite[] pinataList;
+    public Image pinataImage;
+    public GameObject gameOverScreen;
+    public GameObject winnerWindow; 
+    public GameObject loserWindow;
+
+    public Animator paloAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pinataImage.GetComponent<Image>();
+        pinataImage.sprite = pinataList[Random.Range(0, 2)];
+
+        numPulsacionesNecesarias = Random.Range(10, 20);
+        numPiñata = 5;
+
+        pinatasLeftText.text = numPiñata.ToString();
+        pinatasDestroyedText.text = numPiñataDestruida.ToString();
+
+        StartCoroutine(StartTimer());
+    }
+
+    void Update()
+    {
+        
+        if (startTimer == true)
+        {
+            timer += Time.deltaTime;
+            timerText.text = timer.ToString();
+        }
+
+        if (timer >= stopTime)
+        {
+            startTimer = false;
+            gameOverScreen.SetActive(true);
+            loserWindow.SetActive(true);
+        }
+
+        else if (numPiñataDestruida >= numPiñata)
+        {
+            startTimer = false;
+            gameOverScreen.SetActive(true);
+            winnerWindow.SetActive(true);
+        }
+
+    }
+    public void RomperPiñata()
+    {
+
+
+        if (pinataImage.sprite == burro)
+        {
+            pinataImage.sprite = burroRoto;
+        }
+
+        else if (pinataImage.sprite == estrella)
+        {
+            pinataImage.sprite = estrellaRota;
+        }
+        else if (pinataImage.sprite == pinata)
+        {
+            pinataImage.sprite = pinataRota;
+        }
+    }
+    public void OnButton(InputValue input)
+    {
+        paloAnimator.SetTrigger("dale");
+        numPulsaciones++;
+        if (numPulsaciones >= numPulsacionesNecesarias)
+        {
+            numPulsacionesNecesarias = Random.Range(10, 20);
+            RomperPiñata();
+            numPulsaciones = 0;
+            numPiñataDestruida++;
+            pinatasDestroyedText.text = numPiñataDestruida.ToString();
+            StartCoroutine(ChangePiñata());
+        }
+    }
+
+    public IEnumerator ChangePiñata()
+    {
+        yield return new WaitForSeconds(1.5f);
+        pinataImage.sprite = pinataList[Random.Range(0, 2)];
+
+    }
+
+    public IEnumerator StartTimer()
+    {
+        yield return new WaitForSeconds(5f);
+        startTimer = true;
+
+    }
+
+    public void TimerDone()
+    {
+
+    }
+}
