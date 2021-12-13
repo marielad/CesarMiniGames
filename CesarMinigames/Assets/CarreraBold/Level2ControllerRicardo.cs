ea0dea78715df883f9c405b1be8c7bfdef93a706
Ricardo <71872702+Ricardogg1999@users.noreply.github.com>
2021-12-13 13:04:39 +0000

    Cambiado movimiento del jugador y puesto animaciones
    terminado animaciones de los enemigos y el movimiento del jugador falta sistema de derrota

---
+++ CesarMinigames/Assets/CarreraBold/Level2ControllerRicardo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Level2ControllerRicardo : MonoBehaviour
+{
+
+    Rigidbody rg;
+    public float jumpForce = 5f;
+    void Awake()
+    {
+        rg = GetComponent<Rigidbody>();
+    }
+
+
+    public void PressedButton(InputAction.CallbackContext callback)
+    {
+        if (callback.performed)
+        {
+            //rg.transform.rotation(180f, 0f, 0f);
+            rg.AddForce(Vector3.right * jumpForce, ForceMode.Impulse);
+            rg.gameObject.transform.Rotate(0, 180, 0);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name.Equals("FinishLine"))
+        {
+            StartCoroutine(GameController.instance.MiniGameSuceeded());
+
+
+        }
+    }
+}
--- CesarMinigames/Assets/CarreraBold/MovimientoEnemigos.cs
+++ CesarMinigames/Assets/CarreraBold/MovimientoEnemigos.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-public class Level2ControllerRicardo : MonoBehaviour
+using UnityEngine.UI;
+
+public class MovimientoEnemigos : MonoBehaviour
 {
 
     Rigidbody rg;
-    public float jumpForce = 5f;
+    
     void Awake()
     {
         rg = GetComponent<Rigidbody>();
@@ -17,9 +19,9 @@ public class Level2ControllerRicardo : MonoBehaviour
     {
         if (callback.performed)
         {
-            //rg.transform.rotation(180f, 0f, 0f);
-            rg.AddForce(Vector3.right * jumpForce, ForceMode.Impulse);
-            //transform(transform.rotation, 180, 0, 0, Time.deltaTime);
+            
+            rg.gameObject.transform.Rotate(0, 180, 0);
+
         }
     }
 
@@ -27,6 +29,7 @@ public class Level2ControllerRicardo : MonoBehaviour
     {
         if (other.gameObject.name.Equals("FinishLine"))
         {
+
             StartCoroutine(GameController.instance.MiniGameSuceeded());
 
 
