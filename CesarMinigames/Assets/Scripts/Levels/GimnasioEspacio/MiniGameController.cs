0ded5b1e7a7524afb2b7513d3d5cce5d756bfe6f
Cesat089 <cesare.cabelloa@gmail.com>
2021-12-13 09:51:32 +0000

    Sistema de sonido y animación.
    He conseguido completar el sistema de sonido para que suene un ruido al tocar el espacio y he agregado una animación al personaje que se activa cada vez que el espacio es pulsado.

--- CesarMinigames/Assets/Scripts/Levels/GimnasioEspacio/MiniGameController.cs
+++ CesarMinigames/Assets/Scripts/Levels/GimnasioEspacio/MiniGameController.cs
@@ -10,13 +10,37 @@ public class MiniGameController : MonoBehaviour
     public float currentBarraCantidad;
     public float maxBarraCapacidad;
 
+    public Sprite mus1;
+    public Sprite mus2;
+    public Sprite mus3;
+    public Sprite presion;
+    public Sprite nopres;
+
     public Slider barra;
+
+    public GameObject personaje;
+    private Image person;
+    public GameObject boton;
+    private Image bot;
+
+    public float tempo1 = 1f;
+    public float tempo2 = 1f;
+
+    public bool sec1 = false;
+    public bool sec2 = false;
+
+    private Animator pesas;
 
     private void Awake()
     {
         currentBarraCantidad = 0f;
         barra.value = currentBarraCantidad;
         barra.maxValue = maxBarraCapacidad;
+
+        person = personaje.GetComponent<Image>();
+        bot = boton.GetComponent<Image>();
+        pesas = personaje.GetComponent<Animator>();
+        sec1 = true;
     }
 
     // Update is called once per frame
@@ -24,9 +48,50 @@ public class MiniGameController : MonoBehaviour
     {
         if (currentBarraCantidad >= maxBarraCapacidad)
         {
+            
             StartCoroutine(GameController.instance.MiniGameSuceeded());
         }
-        
+
+        if(currentBarraCantidad < (maxBarraCapacidad * 0.5f))
+        {
+            person.sprite = mus1;
+        }
+
+        if (currentBarraCantidad > (maxBarraCapacidad * 0.5f))
+        {
+            person.sprite = mus2;
+        }
+
+        if(currentBarraCantidad > (maxBarraCapacidad * 0.99f))
+        {
+            person.sprite = mus3;
+        }
+
+        if(sec1 == true)
+        {
+            tempo1 = tempo1 - Time.deltaTime;
+        }
+
+        if(tempo1 <= 0f)
+        {
+            sec1 = false;
+            tempo1 = 1f;
+            bot.sprite = presion;
+            sec2 = true;
+        }
+
+        if (sec2 == true)
+        {
+            tempo2 = tempo2 - Time.deltaTime;
+        }
+
+        if (tempo2 <= 0f)
+        {
+            sec2 = false;
+            tempo2 = 1f;
+            bot.sprite = nopres;
+            sec1 = true;
+        }
     }
 
     public void PressedButton(InputAction.CallbackContext callback)
@@ -35,6 +100,8 @@ public class MiniGameController : MonoBehaviour
         {
             currentBarraCantidad = currentBarraCantidad + espacioCantidad;
             barra.value = currentBarraCantidad;
+            GimnasioSFX.instance.GimnasioToque();
+            pesas.Play("Levantamiento");
         }
     }
 }
