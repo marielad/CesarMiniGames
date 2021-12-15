6da89aea436e1b33378f7950b3e5fdcd67ec4c33
Cheomm88 <chopids@gmail.com>
2021-12-15 11:14:32 +0000

    Improvements in fail screen
    Now hearts are shown during the fail screen.
    FailedScreen now has a shake for the cross.

--- CesarMinigames/Assets/Scripts/UI/FailedScreen.cs
+++ CesarMinigames/Assets/Scripts/UI/FailedScreen.cs
@@ -9,9 +9,34 @@ public class FailedScreen : MonoBehaviour
     public float durationShowed = 2.0f;
     public GameObject crossIcon;
 
+    [SerializeField] private Vector2 _cameraShake;
+    Vector3 _initialScale, _initialPos;
+
+
+    private void Awake()
+    {
+        _initialScale = crossIcon.transform.localScale;
+        _initialPos = crossIcon.transform.localPosition;
+    }
     void OnEnable()
     {
         LeanTween.scale(crossIcon, new Vector3(5f, 5f, 5f), 0.5f).setLoopPingPong();
+        LeanTween.moveLocalX(crossIcon, _cameraShake.x, 0.05f).setLoopPingPong();
+        LeanTween.moveLocalY(crossIcon, _cameraShake.y, 0.05f).setLoopPingPong(); 
+    }
+
+    private void OnDisable()
+    {
+        LeanTween.cancel(crossIcon);
+        crossIcon.transform.localScale = _initialScale;
+        crossIcon.transform.localPosition = _initialPos;
+
+    }
+
+    void EndShake()
+    {
+       // LeanTween.move(crossIcon, _initialPos, 0.00f);
+        LeanTween.scale(crossIcon, _initialScale, 0.01f);
     }
 
     // Update is called once per frame
